ec70b71b6cad952ee1b33383883f143bda5f0ca7
navjack <jack.mangano@gmail.com>
2022-01-18 23:22:11 -0500

    Update journal.cpp
    comment out the new bit because I have no idea how to do variables.
    it wont let me access variables from other sections of the code?
    how fix?

--- journal.cpp
+++ journal.cpp
@@ -28,42 +28,42 @@ private:
 
 };
 
-void askuserworkingdirectory();
+//void askuserworkingdirectoryfunc();
 void theJournal();
 
 int main()
 {
     SetupVars vars;
-    askuserworkingdirectory();
+//  askuserworkingdirectoryfunc();
     theJournal();
     return 0;
 }
 
-void askuserworkingdirectory()
-{
-    class SetupVars  ( SetupVars );
-    cout << "What folder would you like to save the journal file to?" << endl;
-    //string workingDirectory;
-    getline(cin, SetupVars.workingDirectory);
-    string journalFile = SetupVars.workingDirectory + "/journal.md";
-    //ifstream journal;
-    SetupVars.journal.open(journalFile);
-    if (SetupVars.journal.is_open()) 
-    {
-        cout << "journal.md file already exists in this folder" << endl;
-    }
-    else 
-    {
-        //ofstream newJournalFile;
-        SetupVars.newJournalFile.open(journalFile);
-        SetupVars.newJournalFile << "";
-        SetupVars.newJournalFile.close();
-    }
-}
+//void askuserworkingdirectoryfunc()
+//{
+//    class SetupVars(SetupVars);
+//    cout << "What folder would you like to save the journal file to?" << endl;
+//    //string workingDirectory;
+//    getline(cin, SetupVars.workingDirectory);
+//    string userjournalFile = SetupVars.workingDirectory + "/journal.md";
+//    //ifstream journal;
+//    SetupVars.journal.open(userjournalFile);
+//    if (SetupVars.journal.is_open())
+//    {
+//        cout << "journal.md file already exists in this folder" << endl;
+//    }
+//    else
+//    {
+//        //ofstream newJournalFile;
+//        SetupVars.newJournalFile.open(userjournalFile);
+//        SetupVars.newJournalFile << "";
+//        SetupVars.newJournalFile.close();
+//    }
+//}
 
 void theJournal()
 {
-    class SetupVars  ( SetupVars );
+    class SetupVars  (SetupVars);
     SetupVars.journal.open(SetupVars.journalFile);
     SetupVars.currentDate = to_string(SetupVars.now -> tm_year + 1900) + "/" + to_string(SetupVars.now -> tm_mon + 1) + "/" + to_string(SetupVars.now -> tm_mday);
     SetupVars.currentTime = to_string(SetupVars.now -> tm_hour) + ":" + to_string(SetupVars.now -> tm_min) + ":" + to_string(SetupVars.now -> tm_sec);
