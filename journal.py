ecbe3fb9ff09eb0b99ae40b553a0e24653d5b22e
navjack <jack.mangano@gmail.com>
2022-02-02 16:52:39 -0500

    Update journal.py
    using a variable the right way i think now for the journal file path.
    displays the contents of the journal in reverse order after clearing the console.

--- journal.py
+++ journal.py
@@ -1,22 +1,24 @@
 import datetime
+import os
 
-# read each line of the file "journal.md" into a list of strings as a variable called "existing_entries"
-with open("journal.md", "r") as EXISTING_JOURNAL_FILE:
-    existing_entries = EXISTING_JOURNAL_FILE.readlines()
+journal_file = "/Volumes/ext/journal-gitea/NavJack/To Do/Untracked To Do.md"
 
-# ask the user "What did you accomplish just now?" and save the answer as a variable called "new_entry"
 new_entry = input("What did you accomplish just now?\n")
 
-# get the current date and time and save it as a variable called "date_and_time" (YEAR/MONTH/DAY - HOUR:MINUTE:SECOND AM/PM)
 date_and_time = datetime.datetime.now().strftime("%Y/%m/%d - %I:%M:%S %p")
 
-# append the new entry to the start of the list of existing entries
+with open(journal_file, "r") as open_journal:
+    existing_entries = open_journal.readlines()
+
 existing_entries.insert(0, "* " + date_and_time + " - " + new_entry + "\n")
 
-# write the updated list of entries to the file "journal.md"
-with open("journal.md", "w") as EXISTING_JOURNAL_FILE:
-    EXISTING_JOURNAL_FILE.writelines(existing_entries)
+with open(journal_file, "w") as open_journal:
+    open_journal.writelines(existing_entries)
 
-# print "journal.md" to the screen
-with open("journal.md", "r") as EXISTING_JOURNAL_FILE:
-    print(EXISTING_JOURNAL_FILE.read())
+os.system('cls' if os.name == 'nt' else 'clear')
+
+with open(journal_file, "r") as open_journal:
+    entries = open_journal.readlines()
+    entries.reverse()
+    for entry in entries:
+        print(entry.rstrip())
