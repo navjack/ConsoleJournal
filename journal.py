9fb37b86a29ccb08aca168df314be773402289cb
navjack <jack.mangano@gmail.com>
2022-02-02 17:30:50 -0500

    Update journal.py
    and fixed. it saves the right thing in the right order and it displays before it saves so we can print the new entry with fancy colors.

--- journal.py
+++ journal.py
@@ -17,15 +17,15 @@ with open(journal_file, "r") as open_journal:
 
 entries.insert(0, "* " + date_and_time + " - " + new_entry + "\n")
 
-with open(journal_file, "w") as open_journal:
-    open_journal.writelines(entries)
-
 os.system("cls" if os.name == "nt" else "clear")
 
-with open(journal_file, "r") as open_journal:
-    entries = open_journal.readlines()
-    entries.reverse()
-    for lines in entries:
+with open(journal_file, "r") as open_journal_display:
+    entries_display = open_journal_display.readlines()
+    entries_display.reverse()
+    for lines in entries_display:
         print(lines.rstrip())
 
 print("* " + color_date_and_time + " - " + color_new_entry)
+
+with open(journal_file, "w") as open_journal:
+    open_journal.writelines(entries)
