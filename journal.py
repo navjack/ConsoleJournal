a6277d966ec8070e85a6cfe7b7f95a9d909500dc
navjack <jack.mangano@gmail.com>
2022-02-02 17:23:10 -0500

    Update journal.py
    fancy colors when it prints what you entered back at you

--- journal.py
+++ journal.py
@@ -7,16 +7,15 @@ import code
 journal_file = "/Volumes/ext/journal-gitea/NavJack/To Do/Untracked To Do.md"
 
 new_entry = input("What did you accomplish just now?\n")
+color_new_entry = "\033[32m" + new_entry + "\033[0m"
 
 date_and_time = datetime.datetime.now().strftime("%Y/%m/%d - %I:%M:%S %p")
+color_date_and_time = "\033[31m" + date_and_time + "\033[0m"
 
 with open(journal_file, "r") as open_journal:
     entries = open_journal.readlines()
 
 entries.insert(0, "* " + date_and_time + " - " + new_entry + "\n")
-
-with open(journal_file, "w") as open_journal:
-    open_journal.writelines(entries)
 
 os.system("cls" if os.name == "nt" else "clear")
 
@@ -25,3 +24,8 @@ with open(journal_file, "r") as open_journal:
     entries.reverse()
     for lines in entries:
         print(lines.rstrip())
+
+print("* " + color_date_and_time + " - " + color_new_entry)
+
+with open(journal_file, "w") as open_journal:
+    open_journal.writelines(entries)
